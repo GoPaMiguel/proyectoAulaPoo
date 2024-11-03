ea3b92ad726e848b41b3a748994c57f60ad66b41
MiguelDev <migopa0222@gmail.com>
2024-11-02 21:53:23 -0500

    Create DB connection

--- src/main/java/database/ConnectionJDBC.java
+++ src/main/java/database/ConnectionJDBC.java
@@ -1,4 +1,37 @@
 package database;
 
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ConnectionJDBC {
+
+
+    static final String user = PersonalValues.USER.getValue();
+    static final String password = PersonalValues.PASSWORD.getValue();
+    static final String bd = PersonalValues.DBNAME.getValue();
+    static final String ip = PersonalValues.IP.getValue();
+    static final String puerto = PersonalValues.PORT.getValue();
+    static final String url = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd;
+
+    //PoolConnection
+    private static DataSource getDataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(url);
+        ds.setUsername(user);
+        ds.setPassword(password);
+        ds.setInitialSize(5);
+        return ds;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public static void closeConecction(Connection connection) throws SQLException {
+        connection.close();
+    }
 }
