262b6dfb5586521c74a5c045688a22263417f2ab
MiguelDev <migopa0222@gmail.com>
2024-11-09 12:09:21 -0500

    Refactor to view structure

--- src/main/java/view/private/admin/AdministradorPanel.java
+++ src/main/java/view/private/admin/AdministradorPanel.java
@@ -2,10 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package view.admin.privat;
-
-import java.awt.Image;
-import javax.swing.ImageIcon;
+package view.privat.admin;
 
 /**
  *
