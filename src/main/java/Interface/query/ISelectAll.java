8d55699f4a3756448544c9ff6e18a72848e7aa81
MiguelDev <migopa0222@gmail.com>
2024-11-10 17:32:50 -0500

    Create SelectAllUserHandler V1

--- src/main/java/Interface/query/ISelectAll.java
+++ src/main/java/Interface/query/ISelectAll.java
@@ -1,7 +1,8 @@
 package Interface.query;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ISelectAll<T> {
-    public List<T> selectAll();
+    public List<T> selectAll() throws SQLException;
 }
