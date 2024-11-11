85bf4768067d2db454af69f1909abc8e2c8f70dc
MiguelDev <migopa0222@gmail.com>
2024-11-11 15:48:28 -0500

    InsertUserHandler V2 with validate fields and ExistUser

--- src/main/java/service/user/command/InsertUserHandler.java
+++ src/main/java/service/user/command/InsertUserHandler.java
@@ -3,7 +3,10 @@ package service.user.command;
 import Interface.command.IInsert;
 import database.ConnectionJDBC;
 import model.CORE.User;
+import service.user.util.validations.ExistUserHandler;
+import service.user.util.validations.ValidationUserFieldHandler;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -12,6 +15,8 @@ public class InsertUserHandler implements IInsert<User> {
 
     private final static String INSERT = "INSERT INTO people (name, lastName, career, cedula, password, email) VALUES (?,?,?,?,?,?)";
     private Connection connection_transactional;
+    ExistUserHandler existUserHandler;
+    ValidationUserFieldHandler validationUserFieldHandler;
 
     public InsertUserHandler() {
     }
@@ -24,6 +29,12 @@ public class InsertUserHandler implements IInsert<User> {
     @Override
     public void insert(User user) throws SQLException {
         Connection connection = null;
+        boolean UserExists = existUserHandler.exist(user, connection);
+        boolean validationField = validationUserFieldHandler.validate(user);
+        if (UserExists) {
+            JOptionPane.showMessageDialog(null, "Ya existe un usuario con ese id");
+            return;
+        }else if (!validationField) return;
         PreparedStatement preparedStatement = null;
         try {
             connection = connection_transactional != null ? connection_transactional : ConnectionJDBC.getConnection();
@@ -35,7 +46,7 @@ public class InsertUserHandler implements IInsert<User> {
             preparedStatement.setString(5, user.getIdUser());
             preparedStatement.setString(6, user.getEmail());
             System.out.println("preparedStatement.toString() = " + preparedStatement.toString());
-//                preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
         } finally {
             ConnectionJDBC.closeConecction(preparedStatement);
         }
