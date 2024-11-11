6161393ab5518ada52cfa24a98c6294375392fef
MiguelDev <migopa0222@gmail.com>
2024-11-11 02:24:24 -0500

    DeleteUserHandler V2 (With validation in case the user doesn't  exist)

--- src/main/java/service/user/command/DeleteUserHandler.java
+++ src/main/java/service/user/command/DeleteUserHandler.java
@@ -3,7 +3,9 @@ package service.user.command;
 import Interface.command.IDelete;
 import database.ConnectionJDBC;
 import model.CORE.User;
+import service.user.util.validations.ExistUserHandler;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -12,6 +14,7 @@ public class DeleteUserHandler implements IDelete<User> {
 
     private static final String DELETE = "DELETE FROM people WHERE id=?";
     private  Connection connection;
+    private ExistUserHandler existUserHandler;
 
     public DeleteUserHandler() {
     }
@@ -24,7 +27,11 @@ public class DeleteUserHandler implements IDelete<User> {
     public void Delete(User user) throws SQLException {
         Connection cx = null;
         PreparedStatement ps = null;
-
+        Boolean ok = existUserHandler.exist(user, connection);
+        if (!ok) {
+            JOptionPane.showMessageDialog(null, "Usuario no encontrado");
+            return;
+        }
         try {
             cx = (connection != null) ? connection : ConnectionJDBC.getConnection();
             ps = cx.prepareCall(DELETE);
@@ -33,6 +40,5 @@ public class DeleteUserHandler implements IDelete<User> {
         }finally {
             if(ps != null) ConnectionJDBC.closeConecction(ps);
         }
-
     }
 }
