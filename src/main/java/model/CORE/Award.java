7f8845e68d9056a0dcccfd26e5b304bdbfad45ed
MiguelDev <migopa0222@gmail.com>
2024-11-17 19:03:42 -0500

    Add the new constructors (Create, show, find)

--- src/main/java/model/CORE/Award.java
+++ src/main/java/model/CORE/Award.java
@@ -1,10 +1,32 @@
 package model.CORE;
 
+import model.DTO.AwardDTO.CreateAwardDTO;
+import model.DTO.AwardDTO.FindAwardDTO;
+import model.DTO.AwardDTO.ShowAwardDTO;
+
 public class Award {
     private int id;
     private String code;
     private String name;
     private int points;
+
+
+    public Award(CreateAwardDTO createAwardDTO) {
+        this.code = createAwardDTO.code();
+        this.name = createAwardDTO.name();
+        this.points = createAwardDTO.points();
+    }
+
+    public Award(ShowAwardDTO showAwardDTO) {
+        this.code = showAwardDTO.code();
+        this.name = showAwardDTO.name();
+        this.points = showAwardDTO.points();
+        this.id = showAwardDTO.id();
+    }
+
+    public Award(FindAwardDTO findAwardDTO) {
+        this.code = findAwardDTO.code();
+    }
 
     public int getId() {
         return id;
