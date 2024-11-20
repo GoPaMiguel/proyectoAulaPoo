34982217ea699cf8e0df26b323ea3001a75ee35e
MiguelDev <migopa0222@gmail.com>
2024-11-20 16:22:15 -0500

    Award controller ready V1

---
+++ src/main/java/controller/AwardController.java
@@ -0,0 +1,146 @@
+package controller;
+
+import database.ConnectionJDBC;
+import model.CORE.Award;
+import model.DTO.AwardDTO.CreateAwardDTO;
+import model.DTO.AwardDTO.FindAwardDTO;
+import service.award.command.DeleteAwardHandler;
+import service.award.command.InsertAwardHandler;
+import service.award.query.FindAwardByIdHandler;
+import service.award.query.SelectAllAwardsHandler;
+import service.award.util.help.ShowAndCreateAwardTable;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AwardController {
+
+    public static void CreateAwardController(CreateAwardDTO createAwardDTO) {
+        Connection con = null;
+        try {
+            con = ConnectionJDBC.getConnection();
+            InsertAwardHandler insertAwardHandler = new InsertAwardHandler(con);
+            con.setAutoCommit(false);
+            insertAwardHandler.insert(new Award(createAwardDTO));
+            con.commit();
+            JOptionPane.showMessageDialog(null, "Award Created");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Cannot insert Award, because: " + e.getMessage());
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        } finally {
+            if (con != null) {
+                try {
+                    ConnectionJDBC.closeConecction(con);
+                } catch (SQLException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
+            }
+
+
+        }
+
+    }
+
+    public static void DeleteAwardController(FindAwardDTO findAwardDTO) {
+        Connection con = null;
+        try {
+            con = ConnectionJDBC.getConnection();
+            con.setAutoCommit(false);
+            DeleteAwardHandler deleteAwardHandler = new DeleteAwardHandler(con);
+            deleteAwardHandler.Delete(new Award(findAwardDTO));
+            con.commit();
+            JOptionPane.showMessageDialog(null, "Award Deleted");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Cannot delete Award, because: " + e.getMessage());
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+
+        } finally {
+            if (con != null) {
+                try {
+                    ConnectionJDBC.closeConecction(con);
+                } catch (SQLException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public static Award GetAwardController(FindAwardDTO findAwardDTO) {
+        Connection con = null;
+        Award award = null;
+        try {
+            con = ConnectionJDBC.getConnection();
+            con.setAutoCommit(false);
+            FindAwardByIdHandler findAwardByIdHandler = new FindAwardByIdHandler(con);
+            award =  findAwardByIdHandler.findById(new Award(findAwardDTO));
+            con.commit();
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }finally {
+            if (con != null) {
+                try {
+                    ConnectionJDBC.closeConecction(con);
+                } catch (SQLException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
+            }
+        }
+        return award;
+    }
+
+    public static List<Award> GetAllAwardController() {
+        Connection con = null;
+        List<Award> awards = new ArrayList<>();
+
+        try {
+            con = ConnectionJDBC.getConnection();
+            con.setAutoCommit(false);
+            SelectAllAwardsHandler selectAllAwardsHandler = new SelectAllAwardsHandler(con);
+            awards = selectAllAwardsHandler.selectAll();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }finally {
+            if (con != null) {
+                try {
+                    ConnectionJDBC.closeConecction(con);
+                } catch (SQLException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
+            }
+        }
+        return awards;
+    }
+
+    public static void ShowAwardController(JTable table) {
+        Connection con = null;
+        List<Award> awards = GetAllAwardController();
+        if (awards.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "No Awards Found");
+        }else {
+            ShowAndCreateAwardTable showAndCreateAwardTable = new ShowAndCreateAwardTable();
+            showAndCreateAwardTable.showTable(table, awards);
+        }
+    }
+}
