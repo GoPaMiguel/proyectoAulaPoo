3eaa7cc5c960f119e6a66b3f343aab1948688aef
MiguelDev <migopa0222@gmail.com>
2024-11-20 18:49:19 -0500

    Logic and response login

---
+++ src/main/java/service/auth/LoginAuth.java
@@ -0,0 +1,19 @@
+package service.auth;
+
+import model.CORE.User;
+import service.user.query.LoginUserHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class LoginAuth {
+    public static String login(User user, Connection con) throws SQLException {
+
+        if (user.getIdUser().equals("sudo") && user.getPassword().equals("sudo")) return "admin";
+        LoginUserHandler handler = new LoginUserHandler(con);
+        User userDB = handler.login(user);
+        if (user.getIdUser().equals(userDB.getIdUser()) && user.getPassword().equals(userDB.getPassword())) return "user";
+
+        return "NN";
+    }
+}
