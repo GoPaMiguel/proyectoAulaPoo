a7725c67f447189e185130a6acf78239d7fc45e2
MiguelDev <migopa0222@gmail.com>
2024-11-22 17:59:01 -0500

    Show and update profile dto

---
+++ src/main/java/model/DTO/userDTO/ShowProfileDTO.java
@@ -0,0 +1,4 @@
+package model.DTO.userDTO;
+
+public record ShowProfileDTO(String name, String lastName, String email, String password, String career, String cedula, int points) {
+}
---
+++ src/main/java/model/DTO/userDTO/UpdateProfileDTO.java
@@ -0,0 +1,4 @@
+package model.DTO.userDTO;
+
+public record UpdateProfileDTO(String name, String lastName, String email, String password, String career, String cedula) {
+}
